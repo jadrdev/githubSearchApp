8626c2319a6d36ef17993448710dde202d4aa61c
JadrDev <jardev@Mac-mini-de-JadrDev.local>
2023-11-22 11:20:17 +0000

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: [
+    '@testing-library/react-native/cleanup-after-each',
+    './jest.setup.js',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
+  ],
+  testEnvironment: 'node',
 };
